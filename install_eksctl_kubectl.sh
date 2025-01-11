81523ea16edb6b9c4ab3163c620fbb0e439ae17a
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-11 22:36:29 +0530

    k8s workstation

--- install_eksctl_kubectl.sh
+++ install_eksctl_kubectl.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp &>>$LOGFILE
-sudo mv /tmp/eksctl /usr/local/bin &>>$LOGFILE
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp 
+sudo mv /tmp/eksctl /usr/local/bin 
 eksctl version
 
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl &>>$LOGFILE
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl 
 sudo chmod +x ./kubectl
 sudo mv kubectl  /usr/local/bin/
 kubectl version --client
 
-eksctl create cluster --config-file=eks.yaml &>>$LOGFILE
-
+eksctl create cluster --config-file=eks.yaml
