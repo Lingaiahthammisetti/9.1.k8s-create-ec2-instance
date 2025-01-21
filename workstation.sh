da888dc367e2a14f41c86e1a44f8ccf23750807e
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-01-21 23:49:06 +0530

    k8s workstation ec2

--- workstation.sh
+++ workstation.sh
@@ -1,55 +1,12 @@
 #!/bin/bash
-USERID=$(id -u)
-TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-echo  -e "$G Script started executing at:$TIMESTAMP $N"
-
-VALIDATE(){
-if [ $1 -ne 0 ]
-then 
-   echo -e "$R $2... FAITURE $N"
-   exit 1
-else
-   echo -e "$G $2..  SUCCESS $N"
-fi
-}
-#checking root user or not.
-if [ $USERID -ne 0 ]
-then
-   echo -e "$R Please run this script with root access $N"
-   exit 1
-else
-   echo -e "$G You are super user. $SCRIPT_NAME"
-
-fi 
-
-yum install yum-utils -y &>>$LOGFILE
-VALIDATE $? "Installing utils packages"
-
-yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo &>>$LOGFILE
-VALIDATE $? "adding Docker repo"
-
-yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y &>>$LOGFILE
-VALIDATE $? "Installing docker"
-
-systemctl start docker &>>$LOGFILE
-VALIDATE $? "Starting docker"
-
-systemctl enable docker &>>$LOGFILE
-VALIDATE $? "Enabling Docker"
-
-usermod -aG docker ec2-user &>>$LOGFILE
-VALIDATE $? "Adding ec2-user to docker group as secondary group"
-
-echo -e "$G Logout and login again $N"
-
-
+#Installing Docker
+yum install yum-utils -y 
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo 
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
+systemctl start docker 
+systemctl enable docker 
+usermod -aG docker ec2-user 
 echo "******* Resize EBS Storage ****************"
 # ec2 instance creation request for Docker expense project
 # =============================================
@@ -58,85 +15,48 @@ echo "******* Resize EBS Storage ****************"
 # allow-everything
 # 50 GB
 
-lsblk &>>$LOGFILE
-VALIDATE $? "check the partitions"
+lsblk 
+sudo growpart /dev/nvme0n1 4  #t3.micro used only
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol 
+sudo lvextend -l +50%FREE /dev/RootVG/varVol 
+sudo xfs_growfs / 
+sudo xfs_growfs /var 
 
-sudo growpart /dev/nvme0n1 4 &>>$LOGFILE #t3.micro used only
-VALIDATE $? "growpart to resize the existing partition to fill the available space"
-
-sudo lvextend -l +50%FREE /dev/RootVG/rootVol &>>$LOGFILE
-VALIDATE $? "Extend the Logical Volumes Decide how much space to allocate to each logical volume."
-
-sudo lvextend -l +50%FREE /dev/RootVG/varVol &>>$LOGFILE
-VALIDATE $? "Extend the Logical Volumes Decide how much space to allocate to each logical volume.-2"
-
-sudo xfs_growfs / &>>$LOGFILE
-VALIDATE $? "For the root filesystem:"
-
-sudo xfs_growfs /var &>>$LOGFILE
-VALIDATE $? "For the /var filesystem:"
 
 echo "*************   eksctl installation - start *************"
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp &>>$LOGFILE
-VALIDATE $? "Installing eksctl "
-
-sudo mv /tmp/eksctl /usr/local/bin &>>$LOGFILE
-VALIDATE $? "Moving eksctl from tmp to bin folder"
-
-eksctl version &>>$LOGFILE
-VALIDATE $? "eksctl version"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp 
+sudo mv /tmp/eksctl /usr/local/bin 
+eksctl version 
 echo "*************   eksctl installation - completed *************"
 
 echo "*************   kubectl installation - start *************"
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl &>>$LOGFILE
-VALIDATE $? "Installing kubectl"
-
-sudo chmod +x ./kubectl &>>$LOGFILE
-VALIDATE $? "changing kubectl file permission to execute"
-
-sudo mv kubectl  /usr/local/bin/ &>>$LOGFILE
-VALIDATE $? "Moving kubectl from current folder to bin folder"
-
-kubectl version --client &>>$LOGFILE
-VALIDATE $? "kubectl version "
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl 
+sudo chmod +x ./kubectl 
+sudo mv kubectl  /usr/local/bin/ 
+kubectl version --client 
 echo "*************   kubectl installation - completed *************"
 
 echo "*************kubens Setup - start*************"
-#install Kubens installtion for expense namespace
-sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx &>>$LOGFILE
-VALIDATE $? "kubectx cloned... "
-
-sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens &>>$LOGFILE
-VALIDATE $? "created kubens softlink"
-
-kubens expense &>>$LOGFILE
-VALIDATE $? "moved to default namespace to expense"
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx 
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens 
+kubens expense 
 echo "*************kubens Setup - completed*************"
 
 echo "*************Install K9s - Start*************"
 #Installing K9s
-
-curl -sS https://webinstall.dev/k9s | bash &>>$LOGFILE
-VALIDATE $? "K9s Installed"
-
+curl -sS https://webinstall.dev/k9s | bash 
 k9s --version
-VALIDATE $? "K9s Vesion" &>>$LOGFILE
 echo "*************Install K9s - completed*************"
 
 
 echo "*************Install Helm - Start*************"
 #Installing Helm in Kubernetes
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 &>>$LOGFILE
-VALIDATE $? "Downloading the Helm"
-
-chmod 700 get_helm.sh &>>$LOGFILE
-VALIDATE $? "changing helm file permission to 700"
-
-./get_helm.sh &>>$LOGFILE
-VALIDATE $? "Executing the Helm file"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 
+chmod 700 get_helm.sh 
+./get_helm.sh 
 echo "*************Install Helm - completed*************"
 
 # echo "*************   eksctl cluster creation started *************"
-# eksctl create cluster --config-file=eks.yaml &>>$LOGFILE
+# eksctl create cluster --config-file=eks.yaml 
 # VALIDATE $? "eksctl cluster creation process"
 # echo "*************   eksctl cluster creation completed *************"
