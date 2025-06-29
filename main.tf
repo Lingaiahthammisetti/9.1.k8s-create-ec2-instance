bcccfd72ac85f2e15adcd1b7d88e73263d5b939d
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-29 13:17:39 +0530

    k8s create ec2 instance

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ resource "aws_instance" "k8s_workstation" {
     volume_type = "gp2"  # General Purpose SSD (you can change the volume type if needed)
     delete_on_termination = true  # Automatically delete the volume when the instance is terminated
     }
-    
     tags = {
         Name = "K8s-Workstation"
     }
@@ -22,22 +21,3 @@ resource "aws_route53_record" "k8s_r53" {
     records = [aws_instance.k8s_workstation.public_ip]
     allow_overwrite = true
 }
-
-# resource "aws_instance" "expense_frontend" {
-#     ami           = data.aws_ami.rhel_info.id
-#     instance_type = var.frontend_instance.instance_type
-#     vpc_security_group_ids = [var.allow_all]
-#     user_data = file("${path.module}/install_frontend.sh")
-
-#     tags = {
-#         Name = "frontend"
-#     }
-# }
-# resource "aws_route53_record" "frontend_r53" {
-#     zone_id = var.zone_id
-#     name    = "frontend.${var.domain_name}"
-#     type    = "A"
-#     ttl     = 1
-#     records = [aws_instance.expense_frontend.public_ip]
-#     allow_overwrite = true
-# }
