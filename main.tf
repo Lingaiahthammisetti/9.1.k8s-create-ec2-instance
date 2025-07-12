2f1de55a0d0dbd9c70daac748a5d3fa80796e342
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-12 10:07:17 +0530

    k8s create ec2 instance

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 resource "aws_instance" "k8s_server_instance" {
     ami           = data.aws_ami.rhel_info.id
     instance_type = var.k8s_instance.instance_type
-    vpc_security_group_ids = [var.allow-everything]
-    #user_data = file("workstation.sh")
-    #user_data               = file("${path.module}/install_jenkins_master.sh")
+    vpc_security_group_ids = [var.allow_everything]
+    #user_data  = file("workstation.sh")
+    #user_data  = file("${path.module}/install_jenkins_master.sh")
     root_block_device {
-    volume_size = 100  # Size of the root volume in GB
-    volume_type = "gp2"  # General Purpose SSD (you can change the volume type if needed)
-    delete_on_termination = true  # Automatically delete the volume when the instance is terminated
+        volume_size = 50
+        volume_type = "gp3"
+        iops        = 3000
+        encrypted   = true
     }
     tags = {
         Name = "K8s-ec2-instance"
